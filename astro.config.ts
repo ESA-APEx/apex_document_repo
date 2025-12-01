c8983b5decd0e5f4aa0d92a8aaf709cd7bcc1f26
bramjanssen <brm.janssen@gmail.com>
2025-12-01 14:07:47 +0100

    chore: updated config

---
+++ astro.config.ts
@@ -0,0 +1,32 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+
+const config = {
+  local: {
+    SITE_URL: "http://localhost:4321",
+    BASE_PATH: "",
+  },
+  production: {
+    SITE_URL: "https://esa-apex.github.io/apex_document_repo/",
+    BASE_PATH: "",
+  },
+};
+
+const buildTarget = (process.env.BUILD_TARGET ??
+  "local") as keyof typeof config;
+
+const { SITE_URL, BASE_PATH } = config[buildTarget];
+
+// https://astro.build/config
+export default defineConfig({
+  site: SITE_URL,
+  base: BASE_PATH,
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
